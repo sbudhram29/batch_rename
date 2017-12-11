81ac2c4a7d66a37b8ce463d26f8822e4a07a291a
Sean Budhram <sbudhram@MacBook-Pro.fios-router.home>
2017-12-11 17:49:13 -0500

    Batch Rename

---
+++ substring_rename.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#$1 = image type
+#$2 = season
+#$3 = company
+#$4 = index number starting at 1
+
+for i in *.$1
+do
+  newName=$(echo $i | cut -d'_' -f $4);
+  echo "$i" $2"_$3_$newName".$1;
+done
+
