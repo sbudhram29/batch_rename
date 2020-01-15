3fd722b394a9b596019741f6bca6f02642035c6c
Sean Budhram <sbudhram@carlisle-etcetera.com>
2020-01-15 11:49:15 -0500

    add optional separator

--- substring_rename.sh
+++ substring_rename.sh
@@ -2,10 +2,11 @@
 
 #$1 = image type
 #$2 = index number starting at 1
+#$3 = separator
 
-for i in *.$1
+for i in `ls -r *.$1`
 do
-  newName=$(echo $i | cut -d'_' -f $2);
+  newName=$(echo $i | cut -d${3:--} -f $2);
   mv "$i" "$newName".$1;
 done
 
--- substring_rename_test.sh
+++ substring_rename_test.sh
@@ -2,10 +2,11 @@
 
 #$1 = image type
 #$2 = index number starting at 1
+#$3 = separator [-, _]
 
 for i in *.$1
 do
-  newName=$(echo $i | cut -d'_' -f $2);
+  newName=$(echo $i | cut -d${3:--} -f $2);
   echo "$i" "$newName".$1;
 done
 
